244a85a87ca2159e766d8c5519ae4cde04a53523
Mohammed Shurrab <mohmmedshurrab07@gmail.com>
2023-02-15 22:53:33 +0200

    Admin profile and image update

--- basic/app/Http/Controllers/AdminController.php
+++ basic/app/Http/Controllers/AdminController.php
@@ -23,6 +23,46 @@ class AdminController extends Controller
         $id = Auth::user()->id;
         $adminData = User::find($id);
         return view('admin.admin_profile_view' , compact('adminData'));
+        // $user = auth()->user();
+        // $id = optional($user)->id;
+    
+        // if (!$id) {
+        //     // Handle the case where the user is not authenticated
+        //     return redirect()->route('login');
+        // }
+    
+        // $editData = User::find($id);
+    
+        // if (!$editData) {
+        //     // Handle the case where the user is authenticated but the user record is not found
+        //     return redirect()->back()->withErrors(['error' => 'User not found']);
+        // }
+    
+        // return view('admin.admin_profile_edit', compact('editData'));
     
     } //end method 
+
+    public function editProfile(){
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.admin_profile_edit' , compact('editData'));
+    } //end method
+
+    public function storeProfile(Request $request){
+        $id   = Auth::user()->id;
+        $data = User::find($id);
+        $data->name     = $request->name;
+        $data->email    = $request->email;
+        $data->username = $request->username;
+
+        if($request->file('profile_image')){
+            $file     = $request->file('profile_image');
+            $filename = date('ymdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images') , $filename);
+            $data['profile_image'] = $filename;
+
+    } 
+    $data->save();
+    return redirect()->route('admin.profile');
+}
 }
